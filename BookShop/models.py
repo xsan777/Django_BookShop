32ee04f0c4bd844be51c161ac1b170315b864105
sxan777 <1315969544@qq.com>
2018-01-31 17:48:19 +0800

    第一次

--- BookShop/models.py
+++ BookShop/models.py
@@ -1,5 +1,30 @@
 from django.db import models
 
+
 # Create your models here.
 class Classification(models.Model):
-    cname=models.CharField(max_length=18)
+    class Meta:
+        db_table = 'classification'
+
+    cname = models.CharField(max_length=18)
+
+    def __str__(self):
+        return self.cname
+
+
+class BookName(models.Model):
+    class Meta:
+        db_table = 'bookname'
+
+    bname = models.CharField(max_length=30)
+    bintroduction = models.CharField(max_length=3000)
+    bcomment = models.TextField(default=None)
+    cfname = models.ForeignKey(Classification, on_delete=models.CASCADE)
+
+
+class Chapter(models.Model):
+    class Meta:
+        db_table = 'chapter'
+    clist=models.CharField(max_length=30)
+    ccontent=models.TextField()
+    cbook=models.ForeignKey(BookName, on_delete=models.CASCADE)
