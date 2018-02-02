ffffd16736d494b373a404808692cd0004bc0a6d
sxan777 <1315969544@qq.com>
2018-02-02 22:11:48 +0800

    增加了轮播数据库

--- BookShop/admin.py
+++ BookShop/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from BookShop.models import *
+
+
 # Register your models here.
 # from .models import Classification
 
 @admin.register(Classification)
 class CFAdmin(admin.ModelAdmin):
-    cname=Classification.cname
+    cname = Classification.cname
     cname.short_description = '分类名'
     list_display = ['cname']
 
@@ -13,9 +15,17 @@ class CFAdmin(admin.ModelAdmin):
 @admin.register(BookName)
 class BNAdmin(admin.ModelAdmin):
     list_display = ['bname', 'cfname']
+    exclude = ['bcomment']
+
+
 @admin.register(Chapter)
 class chadmin(admin.ModelAdmin):
-    list_display = ['clist','ccontent','cbook']
+    list_display = ['clist', 'ccontent', 'cbook']
+
+
 @admin.register(Size)
 class Size(admin.ModelAdmin):
     list_display = ['size']
+@admin.register(Lunbo)
+class Lunbo(admin.ModelAdmin):
+    list_display = ['lunbo']
--- BookShop/models.py
+++ BookShop/models.py
@@ -6,7 +6,7 @@ class Classification(models.Model):
     class Meta:
         db_table = 'classification'
 
-    cname = models.CharField(r'分类名',max_length=18, db_column="分类名")
+    cname = models.CharField(r'分类名', max_length=18, db_column="分类名")
 
     def __str__(self):
         return self.cname
@@ -16,7 +16,7 @@ class Size(models.Model):
     class Meta:
         db_table = 'size'
 
-    size = models.CharField(r'字数分类',max_length=18, db_column="字数分类")
+    size = models.CharField(r'字数分类', max_length=18, db_column="字数分类")
 
     def __str__(self):
         return self.size
@@ -26,14 +26,15 @@ class BookName(models.Model):
     class Meta:
         db_table = 'bookname'
 
-    bname = models.CharField(r'书名',max_length=30, db_column="书名", db_index=True)
-    bintroduction = models.CharField(r'简介',max_length=3000, db_column="简介")
-    bcomment = models.TextField(r'评论',null=True,blank=True, db_column="评论")
-    bauthor = models.CharField(r'作者',max_length=18, db_column="作者", db_index=True)
-    bsize = models.IntegerField(r'字数',db_column="字数")
-    bfsize = models.ForeignKey(Size,on_delete=models.CASCADE,db_column="分类字数")
+    bname = models.CharField(r'书名', max_length=30, db_column="书名", db_index=True)
+    bintroduction = models.CharField(r'简介', max_length=3000, db_column="简介")
+    bcomment = models.TextField(r'评论', null=True, blank=True, db_column="评论")
+    bauthor = models.CharField(r'作者', max_length=18, db_column="作者", db_index=True)
+    bsize = models.IntegerField(r'字数', db_column="字数")
+    img = models.ImageField(upload_to='static/bookshop/img')
+    bfsize = models.ForeignKey(Size, on_delete=models.CASCADE, db_column="分类字数")
     cfname = models.ForeignKey(Classification, on_delete=models.CASCADE, db_column="类名")
-    img=models.ImageField(upload_to='static/bookshop/img')
+
     def __str__(self):
         return self.bname
 
@@ -42,11 +43,21 @@ class Chapter(models.Model):
     class Meta:
         db_table = 'chapter'
 
-    clist = models.CharField(r'目录',max_length=30, db_index=True,)
+    clist = models.CharField(r'目录', max_length=30, db_index=True, )
     ccontent = models.TextField(r'内容')
-    creattime = models.DateField(r'创建时间',auto_now_add=True, db_index=True)
+    creattime = models.DateField(r'创建时间', auto_now_add=True, db_index=True)
     cbook = models.ForeignKey(BookName, on_delete=models.CASCADE)
-    views=models.IntegerField(r'点击数',default=0)
+    views = models.IntegerField(r'点击数', default=0)
 
     def __str__(self):
         return self.clist
+
+
+class Lunbo(models.Model):
+    class Meta:
+        db_table = 'lunbo'
+
+    img = models.ImageField(r'轮播图片',upload_to='static/bookshop/img/lunbo')
+
+    def __str__(self):
+        return self.img
